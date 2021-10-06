2af4fe3b2068cdfd9577c595a599304a57375e17
MedtheVorg <68510473+MedtheVorg@users.noreply.github.com>
2021-10-06 23:03:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// const arrow = document.querySelector('.arrow');
+// const question = document.querySelector('.qst');
+// const answer = document.querySelector('.answer');
+// const questionTop = document.querySelectorAll('.question--value');
+
+// const showAnswer = function () {
+//   arrow.classList.toggle('rotate');
+//   answer.classList.toggle('hidden');
+//   question.classList.toggle('font-weight-bold');
+// };
+
+// for (let i = 0; i < questionTop.length; i++)
+//   questionTop[i].addEventListener('click', showAnswer);
